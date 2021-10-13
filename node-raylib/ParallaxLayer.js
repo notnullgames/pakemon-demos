f514bdbb48cebdb600e8c74aef6e2ec770311918
David Konsumer <konsumer@jetboystudio.com>
2021-10-12 23:36:06 -0700

    use single image for parallax layer

--- node-raylib/ParallaxLayer.js
+++ node-raylib/ParallaxLayer.js
@@ -6,11 +6,9 @@ export default class ParallaxLayer {
   constructor (image, speed = 1.0, offsety = 0, offset = 0) {
     this.iloadedimages = typeof image === 'string'
     if (this.iloadedimages) {
-      this.image1 = r.LoadTexture(image)
-      this.image2 = r.LoadTexture(image)
+      this.image = r.LoadTexture(image)
     } else {
-      this.image1 = image
-      this.image2 = image
+      this.image = image
     }
     this.speed = speed
     this.scroll = offset
@@ -19,17 +17,16 @@ export default class ParallaxLayer {
 
   draw () {
     this.scroll += this.speed
-    if (this.scroll <= (-1 * this.image1.width)) {
+    if (this.scroll <= (-1 * this.image.width)) {
       this.scroll = 0
     }
-    r.DrawTextureV(this.image1, r.Vector2(this.scroll, this.offsety), r.WHITE)
-    r.DrawTextureV(this.image2, r.Vector2(this.scroll + this.image1.width, this.offsety), r.WHITE)
+    r.DrawTextureV(this.image, r.Vector2(this.scroll, this.offsety), r.WHITE)
+    r.DrawTextureV(this.image, r.Vector2(this.scroll + this.image.width, this.offsety), r.WHITE)
   }
 
   destroy () {
     if (this.iloadedimages) {
-      r.UnloadTexture(this.image1)
-      r.UnloadTexture(this.image2)
+      r.UnloadTexture(this.image)
     }
   }
 }
