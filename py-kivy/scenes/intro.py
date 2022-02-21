d64b673d09d5faf8d0092baef5c8a095bb9fa0a7
David Konsumer <konsumer@jetboystudio.com>
2022-02-20 16:45:56 -0800

    more kivy scaffoldig

---
+++ py-kivy/scenes/intro.py
@@ -0,0 +1,8 @@
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+
+class Scene(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Rectangle(source="assets/title.png", pos=(89, 120), size=(158, 62))
